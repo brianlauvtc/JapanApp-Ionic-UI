b74fe8b7f1d18db6630621bbf8ca3c18dc5b6773
KCL47 <KCL47@BRIAN_LAU>
2026-06-01 11:08:00 +0800

    remove useless code

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TransactionItemComponent } from './transaction-item/transaction-item.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-import { ChartCardComponent } from './chart-card/chart-card.component';
 
 @NgModule({
   declarations: [
-    TransactionItemComponent,
-    SearchBarComponent,
-    ChartCardComponent
+    TransactionItemComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    TransactionItemComponent,
-    SearchBarComponent,
-    ChartCardComponent
+    TransactionItemComponent
   ]
 })
 export class SharedComponentsModule { }
